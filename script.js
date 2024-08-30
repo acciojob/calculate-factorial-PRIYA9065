99234ba71af8a05c8298e4ab535a4ba8a36a35a8
PRIYA9065 <147863167+PRIYA9065@users.noreply.github.com>
2024-08-30 16:45:45 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 //your JS code here. If required.
+
+function factorial(){
+	let fact = 1;
+	for(let i = 1; i <= number; i++){
+		fact = fact * i;
+	}
+	alert("The factorial of "+ number + " is " + fact);
+}
+const number = prompt("Enter the number whose factorial to be calculated: ");
+factorial(number);
+
